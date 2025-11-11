06c6c0697f0a444325d3ab5af8e673f18efba61b
Arsalan <arsalanbashir831@gmail.com>
2025-11-11 18:55:38 +0500

    delete(auth-page): remove unused login page component
    - Deleted the auth page component that previously handled redirection to the login route, as it is no longer needed.

--- src/app/(auth)/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/(auth)/page.tsx
-import { ROUTES } from "@/constants/routes";
-import { redirect } from "next/navigation";
-export default function Page() {
-  redirect(ROUTES.AUTH.LOGIN); // or any canonical route
-}
